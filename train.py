e690b823c8a5d5936830b0e3ebe28c7806c1c937
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-18 14:08:02 +0530

    "Added numpy import and usage, modified tokenization and data preparation in train.py, updated nltk_utils import"

--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import json
+import numpy as np
+
 from nltk_utils import tokenize, stem, bag_of_words
+
+print(np.__version__)
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -18,4 +22,18 @@ for intent in intents['intents']:
 
 ignore_words = ['?', '!', '.', ',']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
-print(all_words)
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+print(tags)
+
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    lable = tags.index(tag)
+    y_train.append(lable)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
