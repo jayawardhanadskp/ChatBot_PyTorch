717dd98c42a7f8cdfd5b3dcaee3040a15fa33556
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-18 11:48:08 +0530

    tocanized train model words

---
+++ train.py
@@ -0,0 +1,20 @@
+import json
+from nltk_utils import tokenize, stem, bag_of_words
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w) # extend the list
+        xy.append((w, tag)) # store the pattern and tag
+
+ignore_words = ['?', '!', '.', ',']
+print(all_words)
