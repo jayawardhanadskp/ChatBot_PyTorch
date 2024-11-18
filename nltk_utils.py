e60c363688f5593718fffff9bcaa828e56f8258e
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-18 14:23:26 +0530

    fixed typo in tokenize function, implemented bag_of_words function, and added sample usage code.

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,19 +1,48 @@
 import nltk
-# nltk.download('punkt')
-# nltk.download('punkt_tab')
+import numpy as np
+
+# nltk.download('punkt')  # Uncomment this line if you haven't downloaded the punkt tokenizer
 
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
-def tokenize(sentaence):
-    return nltk.word_tokenize(sentaence)
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
 
 def stem(word):
     return stemmer.stem(word.lower())
 
-
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    Return a bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise.
+    Example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag = [0, 1, 0, 1, 0, 0, 0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    # Create a bag of words (vector) with 0s for each word that is not in the sentence
+    bag = np.zeros(len(all_words), dtype='float32')
+
+    # If the word exists in the tokenized sentence, mark it with a 1 in the corresponding index
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
+
+# Sample sentence and all_words list
+sentence = "Hello, how are you?"
+tokenized_sentence = tokenize(sentence)
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+
+# Get the bag of words
+bog = bag_of_words(tokenized_sentence, words)
+
+print("Bag of words:", bog)
+
 
 # a = "How long does delivery take?"
 # print(a)
