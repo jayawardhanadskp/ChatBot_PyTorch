be8198956f961f7e407cde9c558e4c1c3fe88f40
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-19 10:37:20 +0530

    "Added torch.utils.data import, created ChatDataset class, and initialized DataLoader"

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 from nltk_utils import tokenize, stem, bag_of_words
 
@@ -39,3 +40,22 @@ for (pattern_sentence, tag) in xy:
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        super().n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+# dataset [idx]
+def __getitem__(self, idx):
+    return self.x_data[idx], self.y_data[idx]
+
+def __len__(self):
+    return self.n_samples
+
+# Hypermeters
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
