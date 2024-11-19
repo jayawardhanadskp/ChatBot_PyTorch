846e16d972db4e55e913b9d9b042d5b987023d70
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-19 12:50:33 +0530

    Chat done

--- chat.py
+++ chat.py
@@ -22,3 +22,29 @@ model_state = data["model_state"]
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
+
+bot_name = "Sam"
+print(f"{'Welcome to chat with {bot_name}':^60}\n\n")
+while True:
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])  # Ensure it is a 2D tensor (1, num_features)
+    X = torch.from_numpy(X).float()  # Fix: Convert NumPy array to tensor and set dtype to float
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I'm not sure I understand what you mean by ")
